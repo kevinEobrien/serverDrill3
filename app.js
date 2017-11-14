0f4719371fc26517c8c3b1c6199e53dccbd29a4d
KEVIN OBRIEN <kevinobrien@KEVINs-MacBook-Air.local>
2017-11-14 14:11:19 -0700

    fixed start

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const people =[
   {
-  id: '1',
-  "fist name": "Alice",
-  "last name": "Zephyr",
-  "hometown": "Seattle",
+    id: 1,
+    "fist name": "Alice",
+    "last name": "Zephyr",
+    "hometown": "Seattle",
   },
   {
-  id: '2',
-  "fist name": "Bob",
-  "last name": "Yellow",
-  "hometown": "Vancouver",
+    id: 2,
+    "fist name": "Bob",
+    "last name": "Yellow",
+    "hometown": "Vancouver",
   },
   {
-  id: '3',
-  "fist name": "Claire",
-  "last name": "Xylitol",
-  "hometown": "Toledo",
+    id: 3,
+    "fist name": "Claire",
+    "last name": "Xylitol",
+    "hometown": "Toledo",
   },
   {
-  id: '4',
-  "fist name": "Daniel",
-  "last name": "Winston",
-  "hometown": "Akron",
+    id: 4,
+    "fist name": "Daniel",
+    "last name": "Winston",
+    "hometown": "Akron",
   },
   {
-  id: '5',
-  "fist name": "Edina",
-  "last name": "Veridas",
-  "hometown": "Witchita",
-  },
-]
+    id: 5,
+    "fist name": "Edina",
+    "last name": "Veridas",
+    "hometown": "Witchita",
+  },];
 
 function getId (data, id){
   for (var i = 0; i < data.length; i++) {
-   if(data[i].id == id){
-     return data[i]
-   }
+    if(data[i].id == id){
+      return data[i];
+    }
   }
-  return null
+  return null;
 }
 
 app.get("/", function(request, response){
   response.json(people);
-})
+});
 
 app.get("/:id", function(request, response){
   var record = getId(people, request.params.id);
